4a68c437dd46e8843a905e56d2e0c5d58447f5f8
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2023-02-06 10:26:27 +0100

    🎨(authentication) parametrize basic authentication
    Login and password can be changed in .env file.

--- src/locustfile.py
+++ src/locustfile.py
@@ -2,17 +2,41 @@
 import json
 from random import randint
 
-from locust import HttpUser, task
+from locust import HttpUser, events, task
 
 
-class LearningLocker(HttpUser):
-    """User simulating requests to the LearningLocker LRS."""
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument(
+        "--lrs-login",
+        type=str,
+        env_var="LOCUST_LRS_LOGIN",
+        default="",
+        help="Login for LRS basic authentication",
+    )
+    parser.add_argument(
+        "--lrs-password",
+        type=str,
+        env_var="LOCUST_LRS_PASSWORD",
+        default="",
+        help="Password for LRS basic authentication",
+    )
 
-    def __init__(self, *args, **kwargs):
+
+class BaseUser(HttpUser):
+    """Base user simulating requests to the LRS."""
+
+    def on_start(self):
         """Initialize user with basic authentication."""
-        super().__init__(*args, **kwargs)
-        self.client.auth = ("AAA", "BBB")
+        self.client.auth = (
+            self.environment.parsed_options.lrs_login,
+            self.environment.parsed_options.lrs_password,
+        )
         self.client.headers.update({"X-Experience-API-Version": "1.0.3"})
+
+
+class PostStatement(BaseUser):
+    """User requesting to POST statements to the LRS."""
 
     @task
     def statements_post(self):
