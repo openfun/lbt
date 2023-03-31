3132b6037a78d699137140c8ead9d50c78cf68cc
Julien Maupetit <julien@maupetit.net>
2023-03-31 17:09:35 -0400

    ⚡️(project) revamp bench run script
    To run a benchmark you should now run:
    
    bin/bench \
        LRS_ROOT_URL \
        PROTOCOL \
        [DURATION] \
        [CONCURRENT_USERS] \
        [STATEMENTS_PER_REQUEST]
    
    e.g.
    
    bin/bench \
        http://AAA:BBB@ralph:8090/xAPI \
        /app/locust/post.py \
        30 \
        1 \
        1

--- src/common/utils.py
+++ src/common/utils.py
@@ -6,15 +6,6 @@ from uuid import uuid4
 from os.path import exists
 
 
-def construct_basic_auth_str(username, password):
-    """Construct Authorization header value to be used in HTTP Basic Auth"""
-    if isinstance(username, str):
-        username = username.encode("latin1")
-    if isinstance(password, str):
-        password = password.encode("latin1")
-    return "Basic " + b64encode(b":".join((username, password))).strip().decode("ascii")
-
-
 class DatasetReader:
     """Read test data from csv file using an iterator."""
 
@@ -25,9 +16,9 @@ class DatasetReader:
 
     def __next__(self):
         payload = []
-        if not exists(f"/mnt/data/set/dataset-{self.index:04d}.json"):
+        if not exists(f"/app/data/set/dataset-{self.index:04d}.json"):
             self.index = 0
-        file_path = f"/mnt/data/set/dataset-{self.index:04d}.json"
+        file_path = f"/app/data/set/dataset-{self.index:04d}.json"
         with open(file_path, encoding="utf8") as xapi_file:
             for line in xapi_file:
                 statement = json.loads(line)
